f05ddedd7efd8b8eb4fb423ef9da4fe2ec70bd8e
AravindKA1 <102651448+AravindKA1@users.noreply.github.com>
2022-05-20 13:34:25 +0530

    CRUD
    Added CRUD Models

--- EmployeeManager/EmployeeManager/Controllers/EmployeeManagerController.cs
+++ EmployeeManager/EmployeeManager/Controllers/EmployeeManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EmployeeManager.Controllers
 {
@@ -11,12 +12,85 @@ namespace EmployeeManager.Controllers
             this.db = db;
         }
 
-        public  IActionResult List()
-        { List<Employee> model= (from e in db.Employees
-                                 orderby e.EmployeeId
-                                 select e).ToList();
+        public IActionResult List()
+        {
+            List<Employee> model = (from e in db.Employees
+                                    orderby e.EmployeeId
+                                    select e).ToList();
             return View(model);
         }
-        
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            FillCountries();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Insert(Employee model)
+        {
+            FillCountries();
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee Added Successfully";
+            }
+            return View(model);
+
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            FillCountries();
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Employee model)
+        {
+            FillCountries();
+            if (ModelState.IsValid)
+            {
+                db.Employees.Update(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee Updated Successfully";
+            }
+            return View(model);
+
+        }
+        [HttpDelete]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var model = db.Employees.Find(id);
+            db.Employees.Remove(model);
+            db.SaveChanges();
+            TempData["Message"] = "Employee Deleted Successfully";
+            return RedirectToAction("List");
+        }
+
+        private void FillCountries()
+        {
+            List<SelectListItem> ListOfCountries = (from c in db.Employees
+                                                    select new
+                                                    SelectListItem()
+                                                    {
+                                                        Text = c.Country,
+                                                        Value = c.Country
+                                                    }).Distinct().ToList();
+            ViewBag.Countries = ListOfCountries;
+        }
+
     }
 }
