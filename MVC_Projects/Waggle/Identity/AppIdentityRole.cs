58f5018853fa2fcf2295f2abb85b70189b2ffc65
AravindKA1 <102651448+AravindKA1@users.noreply.github.com>
2022-07-05 10:04:07 +0530

    ...
    ...

--- MVC_Projects/Waggle/Identity/AppIdentityRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Waggle.Identity
-{
-    public class AppIdentityRole : IdentityRole
-    {
-        public string Decsription { get; set; }
-    }
-}
